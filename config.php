771d8d3dd24c8ed44ad78193ec92cb38bfa99027
sunproggit1 <sundetnet105@gmail.com>
2019-12-11 21:45:01 +0600

    Seccommit

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('SERVERNAME', 'localhost');
 define('USERNAME', 'root');
 define('PASSWORD', '');
-define('DBNAME', 'db_store');
+fine('DBNAME', 'db_store');
 
 $link = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
 if($link === false){
